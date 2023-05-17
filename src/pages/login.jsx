43bad15e352702d5a69a6e79e3b9a72355207d88
sanjay-sudheer <sanjaysudheer2003@gmail.com>
2023-05-17 10:58:28 +0530

    v1

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { MouseParallaxContainer, MouseParallaxChild } from "react-parallax-mouse";
-import apartment from '../images/vector.png'
+import apartment from '../images/apartment.jpg'
 import './login.css'
 export default function login() {
   return (
 
     <MouseParallaxContainer>
         <div className='login'>
-      <div className='head'>
+      <div className='left'>
         <h2>Welcome to PGfinder!</h2>
-      </div>
-      <div className='cont'>
         <p>Find your perfect PG  in seconds with PGfinder
           The ultimate time-saving app for locating nearby PG!</p>
         <button className='login-btn'><img
@@ -18,9 +16,8 @@ export default function login() {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png"
         ></img>Sign in with Google</button>
       </div>
-      <div className='vct'>
-        <MouseParallaxChild factorX={0.05} factorY={0.08}> <img src={apartment} alt="" /></MouseParallaxChild>
-         
+      <div className='right'>
+        <MouseParallaxChild factorX={0.05} factorY={0}><img src={apartment} alt="" /></MouseParallaxChild>
       </div>
 
     </div>
